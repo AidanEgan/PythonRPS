94d5c767c2583277476332fb60a062610d4055dd
“AidanEgan” <aidanegan19@gmail.com>
2022-10-01 16:13:06 -0400

    Removed lines AND fixed a bug! Amazing

--- main.py
+++ main.py
@@ -2,13 +2,10 @@
 import random
 triFecta = ['r','p','s']
 answers = ["DRAW!","YOU LOSER","YOU WIN"]
-while True:
-    userInput = input("Do you choose rock, paper, or scissors? ").lower()[0]
-    if userInput not in triFecta:
-        continue
-    userFinalChoice = [x for x in range(len(triFecta)) if userInput == triFecta[x]][0]
-    comChoice = random.randint(0,2)
-    print([answers[y] for y in range(3) if(triFecta[comChoice-y] == triFecta[userFinalChoice])][0])
-    if input("Do you want to play again? ").lower()[0] != 'y':
-        print("Play again soon")
-        break
+def getInput(inputStr, message=""):
+    return (theInput[0] if theInput else getInput(inputStr, print("I need a response!"))) if (theInput := input(inputStr).lower()) else getInput(inputStr, print("I need a response!"))
+while triFecta:
+    userInput = getInput("Do you choose rock, paper, or scissors? ")
+    userFinalChoice = [x for x in range(len(triFecta)) if userInput == triFecta[x]][0] if userInput in triFecta else -1
+    userFinalChoice = (print([answers[y] for y in range(3) if (triFecta[userInput-y] == triFecta[userFinalChoice])][0]) if userFinalChoice != -1 else -1) if ((userInput := random.randint(0,2)) != -1) else print("Unreachable!")
+    triFecta = (triFecta if (getInput("Do you want to play again? ") == 'y') else print("Play again soon")) if userFinalChoice != -1 else triFecta
